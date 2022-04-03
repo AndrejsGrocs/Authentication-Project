de9bf887807e1bbede6e56d0a11bccaf353969da
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-04-03 10:32:24 +0200

    server updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 
 const app = express()
@@ -12,10 +13,18 @@ dotenv.config()
 app.set('port', process.env.PORT || 3001);
 
 
+app.use(cookieSession({name:'session', keys:['auth'], maxAge:24 * 60 * 60 * 1000}))
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(passport.initialize());
+app.use(passport.session());
 
 
 
